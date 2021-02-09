bf59fc7df734bc7e7dca49eee765db0847d28faa
Alasdair McLeay <alasdair.mcleay@nearform.com>
2021-02-09 16:53:45 +0000

    step 10 - mod from solution

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,17 +1,13 @@
 import S from "fluent-json-schema";
 
 const schema = {
-//   response: {
-//     200: S.array().items(S.object().prop("username", S.string().required())),
-//   },
+  response: {
+    200: S.object().prop("username", S.string().required()),
+  },
 };
 
 export default async function (fastify) {
-    fastify.addHook('preValidation', async (request, response) => {
-        fastify.authenticate(request, response);
-    })
-
-  fastify.get("/user", { schema }, async (request) => {
+  fastify.get("/user", { schema, preValidation:  [fastify.authenticate]}, async (request) => {
     request.log.info("User route");
     return request.user;
   });
